ce88f8c1f3c60e67061cfa7455ab779f353c7c6d
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-04-18 21:39:38 +0100

    update to heroku

--- goldenland/settings.py
+++ goldenland/settings.py
@@ -57,6 +57,14 @@ AUTH_USER_MODEL = 'gdapp.MyUser'
 
 CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
 
+import cloudinary
+
+cloudinary.config( 
+  cloud_name = os.getenv("CLOUDINARY_NAME"), 
+  api_key = os.getenv("CLOUDINARY_API_KEY"), 
+  api_secret = os.getenv("CLOUDINARY_SECRET_KEY") 
+)
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
