9c93a0f353c9e65909056d0519af77d662fa76f1
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-04-19 22:24:16 +0100

    staticfiles upload

--- goldenland/settings.py
+++ goldenland/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'gunicorn',
     'cloudinary_storage',
     'cloudinary',
-    'djangocloudistatic',
+    # 'djangocloudistatic',
     'crispy_forms',
     'gdapp'
 ]
@@ -186,14 +186,14 @@ if DEBUG:
     MEDIA_URL = '/media/'
     MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 else:
-    # DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
     STATIC_ROOT = 'static'
     # STATICFILES_DIRS = (
     # os.path.join(BASE_DIR, 'static'),
     # )
-    STATIC_URL = 'https://res.cloudinary.com/glodenlanders/raw/upload/v1/static/'
-    # STATIC_URL = '/static/'
-    # STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+    # STATIC_URL = 'https://res.cloudinary.com/glodenlanders/raw/upload/v1/static/'
+    STATIC_URL = '/static/'
+    STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
     # other settings, like credentials
