ab7cfd7bdb32ffb2810fda47956b86619086c240
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-06-26 14:46:41 +0100

    update staticfile

--- goldenland/settings.py
+++ goldenland/settings.py
@@ -223,9 +223,9 @@ else:
     # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
     MEDIA_ROOT = 'media'
-    STATIC_URL = '/static/'
+    STATIC_URL = '/golden/'
     MEDIA_URL = '/media/'
-    STATIC_ROOT = 'static'
+    STATIC_ROOT = 'golden'
     STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
     )
@@ -235,7 +235,7 @@ else:
 CLOUDINARY_STORAGE = {
     # other settings, like credentials
     'MEDIA_TAG': 'media',
-    'STATIC_TAG': 'static',
+    'STATIC_TAG': 'golden',
     # 'PUBLIC_ID': '105106107',
     'MAGIC_FILE_PATH': 'magic',
     'PREFIX': '/media/',
